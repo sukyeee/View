f5a6bee66f005f3b842cff08c75f5b61ef136bf5
이수경 <1724271@donga.ac.kr>
2022-11-09 22:39:07 +0900

    2022/11/09

---
+++ WhereIsMyHome/vue-now-ui-kit-master - 복사본/src/router.js
@@ -0,0 +1,86 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Index from './pages/Index.vue';
+import Landing from './pages/Landing.vue';
+import Login from './pages/Login.vue';
+import Profile from './pages/Profile.vue';
+import MainNavbar from './layout/MainNavbar.vue';
+import MainFooter from './layout/MainFooter.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  linkExactActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      components: { default: Index, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: 'black' }
+      }
+    },
+    {
+      path: '/landing',
+      name: 'landing',
+      components: { default: Landing, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: 'black' }
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      components: { default: Login, header: MainNavbar },
+      props: {
+        header: { colorOnScroll: 400 }
+      }
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      components: { default: Profile, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: 'black' }
+      }
+    },
+    {
+			path: '/aptList',
+      name: 'aptList',
+      components: { default: AptList, header: Navbar, footer: Footer },
+      props: {
+        header,
+        footer
+      }
+	},
+  {
+			path: '/eventList',
+      name: 'eventList',
+      components: { default: EventList, header: Navbar, footer: Footer },
+      props: {
+        header,
+        footer
+      }
+	},
+  {
+    path: '/adminPage',
+    name: 'adminPage',
+    components: { default: AdminPage, header: MainNavbar, footer: MainFooter},
+    props: {
+      header,
+      footer
+    }
+  },
+  
+  ],
+  scrollBehavior: to => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
+});
---
+++ WhereIsMyHome/vue-now-ui-kit-master/src/components/index.js
@@ -0,0 +1,48 @@
+import AlertView from "./AlertView";
+import Checkbox from "./Inputs/Checkbox.vue";
+import Radio from "./Inputs/RadioView.vue";
+import FormGroupInput from "./Inputs/formGroupInput.vue";
+import Switch from "./SwitchView.vue";
+import Badge from "./BadgeView";
+import Progress from "./ProgressView.vue";
+import Button from "./ButtonView.vue";
+
+import DropDown from "./DropDown.vue";
+
+import Card from "./Cards/CardView.vue";
+import Collapse from "./Collapse/CollapseView.vue";
+import CollapseItem from "./Collapse/CollapseItem.vue";
+import Navbar from "./Navbar/Navbar";
+import NavLink from "./Navbar/NavLink";
+import NavbarToggleButton from "./Navbar/NavbarToggleButton";
+import TabPane from "./Tabs/TabView.vue";
+import Tabs from "./Tabs/TabsView.vue";
+import Modal from "./ModalView.vue";
+import Parallax from "./ParallaxView.vue";
+import Slider from "./SliderView.vue";
+
+import Pagination from "./PaginationView.vue";
+
+export {
+  AlertView,
+  Collapse,
+  CollapseItem,
+  Checkbox,
+  Switch,
+  Badge,
+  Progress,
+  Pagination,
+  Radio,
+  FormGroupInput,
+  Card,
+  DropDown,
+  Navbar,
+  NavbarToggleButton,
+  NavLink,
+  TabPane,
+  Tabs,
+  Modal,
+  Parallax,
+  Slider,
+  Button,
+};
